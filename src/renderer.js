4631a33106edafc2a268bf9e5a13f6b8bd2db7fd
poxu <poxvuibr@gmail.com>
2015-08-28 18:55:45 +0300

    cross and zer rendering relative to space size

--- src/renderer.js
+++ src/renderer.js
@@ -5,6 +5,10 @@ var Renderer = (function() {
         var len = 50;
         var margin = 10;
 
+        var q = 35/100;
+        var innerOffset = Math.floor(len * q);
+        var innerLen = len - innerOffset * 2;
+
         var renderSpace = function(space, content) {
             var row = space[0];
             var col = space[1];
@@ -12,20 +16,6 @@ var Renderer = (function() {
             ctx.translate(col*(len + margin),row*(len + margin));
 
             ctx.fillStyle = '#bacdd3';
-            /*
-            else if (content === 1) {
-                ctx.fillStyle = 'green';
-            }
-            else if (content === 2) {
-                ctx.fillStyle = 'red';
-            }
-            else if (content === 22) {
-                ctx.fillStyle = 'pink';
-            }
-            else if (content === 11) {
-                ctx.fillStyle = 'lightgreen';
-            }
-            */
 
             ctx.fillRect(0, 0, len, len);    
             ctx.restore();
@@ -34,12 +24,18 @@ var Renderer = (function() {
             ctx.fillStyle = 'white';
             ctx.translate(col*(len + margin),row*(len + margin));
 
+            if(content === 11 || content === 22) {
+                ctx.fillStyle = '#8f4d4b';
+            }
+            
+
             if (content === 1 || content === 11) {
-                ctx.fillRect(10, 10, len - 20, len - 20);    
+                ctx.translate(len / 2, len / 2);
+                ctx.fillRect(-innerLen / 2, -innerLen / 2, innerLen , innerLen );    
             }
             else if (content === 2 || content === 22) {
                 ctx.beginPath();
-                ctx.arc(len / 2, len / 2, (len - 20)/2, 0, 2 * Math.PI, false);
+                ctx.arc(len / 2, len / 2, innerLen / 2, 0, 2 * Math.PI, false);
                 ctx.fill();
             }
 
