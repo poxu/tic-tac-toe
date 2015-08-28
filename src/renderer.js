06586175b9472116c31f5c271093779f8a7a2962
poxu <poxvuibr@gmail.com>
2015-08-28 19:10:06 +0300

    rounded angles for spaces

--- src/renderer.js
+++ src/renderer.js
@@ -17,7 +17,25 @@ var Renderer = (function() {
 
             ctx.fillStyle = '#bacdd3';
 
-            ctx.fillRect(0, 0, len, len);    
+            var width = len;
+            var height = len;
+            var x = 0
+            var y = 0;
+            var radius = 5;
+
+            ctx.beginPath();
+            ctx.moveTo(x + radius, y);
+            ctx.lineTo(x + width - radius, y);
+            ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+            ctx.lineTo(x + width, y + height - radius);
+            ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+            ctx.lineTo(x + radius, y + height);
+            ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+            ctx.lineTo(x, y + radius);
+            ctx.quadraticCurveTo(x, y, x + radius, y);
+            ctx.closePath();
+            ctx.fill();
+
             ctx.restore();
             ctx.save();
 
