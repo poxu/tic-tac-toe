ff329074c6d69a5d75f0482592ace4062723fa34
poxu <poxvuibr@gmail.com>
2015-08-28 18:36:16 +0300

    change background color and draw zeroes and crosses with different color

--- src/renderer.js
+++ src/renderer.js
@@ -9,10 +9,10 @@ var Renderer = (function() {
             var row = space[0];
             var col = space[1];
             ctx.save();
+            ctx.translate(col*(len + margin),row*(len + margin));
 
-            if (content === 0) {
-                ctx.fillStyle = 'black';
-            }
+            ctx.fillStyle = '#bacdd3';
+            /*
             else if (content === 1) {
                 ctx.fillStyle = 'green';
             }
@@ -25,14 +25,39 @@ var Renderer = (function() {
             else if (content === 11) {
                 ctx.fillStyle = 'lightgreen';
             }
+            */
 
-            ctx.fillRect(col*(len + margin), row*(len + margin), len, len);    
+            ctx.fillRect(0, 0, len, len);    
             ctx.restore();
+            ctx.save();
+
+            ctx.fillStyle = 'white';
+            ctx.translate(col*(len + margin),row*(len + margin));
+
+            if (content === 1 || content === 11) {
+                ctx.fillRect(10, 10, len - 20, len - 20);    
+            }
+            else if (content === 2 || content === 22) {
+                ctx.beginPath();
+                ctx.arc(len / 2, len / 2, (len - 20)/2, 0, 2 * Math.PI, false);
+                ctx.fill();
+            }
+
+
+
+            ctx.restore();
+
+
         }
 
         ren.render = function(scene) {
             var rows = scene.length;
             var cols = scene[0].length;
+
+            ctx.save();
+            ctx.fillStyle = 'white';
+            ctx.fillRect(0, 0, 180, 180);
+            ctx.restore();
 
             for(var row = 0; row < rows; ++row) {
                 for(var col = 0; col < cols; ++col) {
